bff2034471e56d4cdbfb69032ecc616f9b9f97ad
RossMeikleham <rossmeikleham@hotmail.co.uk>
2015-02-28 23:20:33 +0000

    Implemented some Brainfuck operations in Scala

--- Brainfuck.scala
+++ Brainfuck.scala
@@ -3,32 +3,75 @@
 object Brainfuck {
 
     class CPU {  
-      var cells:Array[Byte] = new Array[Byte](0x4000) // Memory cells
-      var pc = 0 // Program Counter
-      var dp = 0 // Data Pointer
+      var cells: Array[Byte] = new Array[Byte](0x4000) // Memory cells
+      var pc = 0  // Program Counter
+      var dp = 0  // Data Pointer
+      
+      def CPU(program : Array[Byte]) {
+        cells = program
+      }
 
       def getOp() : Char = { 
         cells(pc).toChar 
       }
+      
+      // Increment Program Counter
+      def incPC() : CPU = {
+        pc += 1
+        this
+      }
+
+      // Increment Data Pointer
+      def incDP() : CPU = {
+        dp += 1
+        this
+      }
+
+      // Decrement Data Pointer
+      def decDP() : CPU = {
+        dp -= 1
+        this
+      }
+      
+      //Increment Byte at the Data Pointer
+      def incByte() : CPU = {
+        cells(dp) = cells(dp) + 1
+        this
+      } 
+
+      //Decrement Byte at the Data Pointer
+      def decByte() : CPU = {
+        cells(dp) = cells(dp) - 1
+        this
+      }
+
+
+
     }
 
     def main(args: Array[String]) {
         
-        var fileName = args(0)         
-        println("Hello World!\n" + fileName); 
+        val fileName = args(0)         
+        println("Hello World!\n" + fileName);
+         
     }
 
-    def run(cpu: CPU) : CPU  = cpu.getOp() match {
-      case '>' => run(cpu)
-      case '<' => run(cpu)
-      case '+' => run(cpu)
-      case '-' => run(cpu)
-      case '[' => run(cpu)
-      case ']' => run(cpu)
-      case ',' => run(cpu)
-      case '.' => run(cpu)
-      case  _  => cpu
+    def run(cpu: CPU) : CPU  = {
 
+      val newCPU = cpu.incPC()
+
+      cpu.getOp() match {
+        case '>' => run(newCPU.incDP())
+        case '<' => run(newCPU.decDP())
+        case '+' => run(newCPU.incByte())
+        case '-' => run(newCPU.decByte())
+        case '[' => run(cpu)
+        case ']' => run(cpu)
+        case ',' => run(cpu)
+        case '.' => run(cpu)
+        case  _  => cpu
+
+    }
     }
     
     
